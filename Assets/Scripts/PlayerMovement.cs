0aee9be414fb27f453c179af2bdba3f6f8d4c782
ap4996 <a.prabuddha@gmail.com>
2020-06-15 23:11:49 +0530

    initial-commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpPower, movementPower, throwPower;
+    public Rigidbody2D character;
+    public Animator animator;
+    public GameObject ballPrefab;
+    public Transform target;
+
+    private Vector3 characterScaleForMovingRight, characterScaleForMovingLeft;
+
+    private void Awake()
+    {
+        characterScaleForMovingRight = transform.localScale;
+        characterScaleForMovingLeft = new Vector3(-characterScaleForMovingRight.x, characterScaleForMovingRight.y, characterScaleForMovingRight.z);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MakeCharacterJump();
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MakeCharacterMoveRight();
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MakeCharacterMoveLeft();
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateBallAndThrowOnTarget();
+        }
+    }
+
+    private void MakeCharacterJump()
+    {
+        character.AddForce(Vector2.up * jumpPower);
+        animator.Play("CharacterJump");
+    }
+
+    private void MakeCharacterMoveRight()
+    {
+        character.transform.localScale = characterScaleForMovingRight;
+        character.AddForce(Vector2.right * movementPower);
+    }
+
+    private void MakeCharacterMoveLeft()
+    {
+        character.transform.localScale = characterScaleForMovingLeft;
+        character.AddForce(Vector2.left * movementPower);
+    }
+
+    private void GenerateBallAndThrowOnTarget()
+    {
+        GameObject ball = Instantiate(ballPrefab, character.position, Quaternion.identity) as GameObject;
+        ball.GetComponent<Rigidbody2D>().AddForce(throwPower * target.position);
+    }
+}
