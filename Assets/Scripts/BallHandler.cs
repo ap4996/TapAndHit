85f62753e110cc9b93e1d2e947549e65af091371
ap4996 <a.prabuddha@gmail.com>
2020-06-20 22:04:09 +0530

    enemy-added

---
+++ Assets/Scripts/BallHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BallHandler : MonoBehaviour
+{
+    private float ballTimer;
+    public float endTimer;
+
+    private void Awake()
+    {
+        ballTimer = 0f;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag.ToLower().Contains("enemy"))
+        {
+            //DestroyBall();
+        }
+    }
+
+    private void DestroyBall()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        ballTimer += Time.deltaTime;
+        if(ballTimer >= endTimer)
+        {
+            DestroyBall();
+        }
+    }
+}
